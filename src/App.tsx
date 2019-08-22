253d074236095e7911719666eb970b502c082851
Andrew Bales <andrewbales@Substantial-MBP18-AS.seattle.substantial.com>
2019-08-21 17:19:11 -0700

    API fetch note

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Search from './Pages/Search';
 import './App.css';
 
+// All KOTH data, fetch:
+// https://3578rll5mf.execute-api.us-west-2.amazonaws.com/dev/query?p=koth
+
 const App: React.FC = () => {
   return (
     <Router>
