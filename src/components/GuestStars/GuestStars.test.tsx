499c0548516c5197dcf7743dd6e204a326cd48cb
agbales <andrew.bales@substantial.com>
2019-08-29 15:55:16 -0700

    small edits

--- src/components/GuestStars/GuestStars.test.tsx
+++ src/components/GuestStars/GuestStars.test.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 // import react-testing methods
-import { render, fireEvent, waitForElement } from '@testing-library/react'
+import { render, fireEvent, waitForElement } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import GuestStars from "./GuestStars";
+import GuestStars from './GuestStars';
 
-const emptyGuests:string[] = [];
-const validGuests:string[] = [
-    "Billy Bob Thornton",
-    "Angelina Jolie",
-    "John Ritter"
+const emptyGuests: string[] = [];
+const validGuests: string[] = [
+  'Billy Bob Thornton',
+  'Angelina Jolie',
+  'John Ritter'
 ];
 
 describe('Guest Stars', () => {
-    it('Renders without breaking', () => {
-        render(<GuestStars guests={emptyGuests}/>);
-        // ReactDOM.unmountComponentAtNode(div);
-    });
+  it('Renders without breaking', () => {
+    render(<GuestStars guests={emptyGuests} />);
+    // ReactDOM.unmountComponentAtNode(div);
+  });
 
-    it('Renders all guest names as list items', () => {
-        const component = <GuestStars guests={validGuests}/>;
+  it('Renders all guest names as list items', () => {
+    const component = <GuestStars guests={validGuests} />;
 
-        const renderedComponent = render(component);
-        // look inside render to see if guests rendered as list item
-        const listItem = document.createElement('li');
-        listItem.innerText = validGuests[1];
-        // listItem.setAttribute('key', '1');
+    const renderedComponent = render(component);
+    // look inside render to see if guests rendered as list item
+    const listItem = document.createElement('li');
+    listItem.innerText = validGuests[1];
+    // listItem.setAttribute('key', '1');
 
-        console.log('renderedComponent: ', renderedComponent);
-        expect(renderedComponent).toContainElement(listItem);
+    console.log('renderedComponent: ', renderedComponent);
+    expect(renderedComponent).toContainElement(listItem);
 
-        // console.log('Post-render: ', component);
-        // expect(JSON.stringify(preRender)).toBe(JSON.stringify(div));
-        // ReactDOM.unmountComponentAtNode(div);
-    });
-    it('Component contains h2 with proper text', () => {
+    // console.log('Post-render: ', component);
+    // expect(JSON.stringify(preRender)).toBe(JSON.stringify(div));
+    // ReactDOM.unmountComponentAtNode(div);
+  });
+  it('Component contains h2 with proper text', () => {});
 
-    });
-
-    describe('Negative cases', () => {
-        it('Renders nothing when Guests is an empty array', () => {
-
-        });
-    });
-
+  describe('Negative cases', () => {
+    it('Renders nothing when Guests is an empty array', () => {});
+  });
 });
--- src/pages/Detail/Detail.test.tsx
+++ src/pages/Detail/Detail.test.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Detail from "./Detail";
-
+import Detail from './Detail';
 
 describe('Detail', () => {
-   it('Renders without breaking', () => {
-       const div = document.createElement('div');
-       ReactDOM.render(<Detail />, div);
-       ReactDOM.unmountComponentAtNode(div);
-   })
+  it('Renders without breaking', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Detail />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
