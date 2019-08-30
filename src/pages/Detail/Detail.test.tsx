d1ca0259fab5ad75628784dcc0e43a61896fe899
James Lee <james@substantial.com>
2019-08-30 09:55:37 -0700

    Fix eslint config

--- src/pages/Detail/Detail.test.tsx
+++ src/pages/Detail/Detail.test.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Detail from "./Detail";
-import {buildEpisode} from '../../test/fixtures'
+import Detail from './Detail';
+import { buildEpisode } from '../../test/fixtures';
 
 describe('Detail', () => {
-   it('Renders without breaking', () => {
-     const episode = buildEpisode()
-       const div = document.createElement('div');
-       ReactDOM.render(<Detail episode={episode} id="foo" programId="bar" />, div);
-       ReactDOM.unmountComponentAtNode(div);
-   })
+  it('Renders without breaking', () => {
+    const episode = buildEpisode();
+    const div = document.createElement('div');
+    ReactDOM.render(<Detail episode={episode} id="foo" programId="bar" />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
-
